60d9eeeba2f29602c9c75d9b4eb6f97cdeb17d22
Gurpreet Singh <refapp.io@gmail.com>
2015-07-06 22:21:06 -0700

    formatting code

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function($routeProvider) 
+{
+	$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
